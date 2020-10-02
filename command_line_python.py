673adfa7360d484da6c69857f1eba79ddb1596f0
Aixi Xu <zczlaxu@ucl.ac.uk>
2020-10-02 12:03:08 +0100

    Others

---
+++ command_line_python.py
@@ -0,0 +1,34 @@
+import sys
+import numpy
+#print('Arguments are: ', sys.argv)
+
+#answer = float(sys.argv[1]) * float(sys.argv[2])
+
+#print("Answer is ", answer)
+
+#action = sys.argv[1]
+#filenames = sys.argv[2:]
+
+#for filename in filenames:
+#    data = numpy.loadtxt(filename, delimiter=',')
+#    if action == '--mean':
+#        print(numpy.mean(data))
+#    elif action == '--min':
+#        print(numpy.min(data))
+#    elif action == '--printname':
+#        print(filename)
+#    else:
+#        print("Command not recognised")
+
+#I'm going to read the arguments
+#x = sys.argv[1]
+
+#I'm going to print a message
+#print(......)
+
+# x + y - z
+x = float(sys.argv[1])
+y = float(sys.argv[2])
+z = float(sys.argv[3])
+
+print(x + y - z)
